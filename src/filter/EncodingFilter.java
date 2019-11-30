4b274ba3cbc91c6efe0a2026bf3ff257db861cf4
GovenZgf <18018593835@163.com>
2019-11-30 21:35:17 +0800

    测试

--- src/filter/EncodingFilter.java
+++ src/filter/EncodingFilter.java
@@ -2,6 +2,8 @@ package filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(filterName = "EncodingFilter")
@@ -10,7 +12,11 @@ public class EncodingFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        request.setCharacterEncoding("utf-8");
         chain.doFilter(req, resp);
+        response.setCharacterEncoding("utf-8");
     }
 
     public void init(FilterConfig config) throws ServletException {
